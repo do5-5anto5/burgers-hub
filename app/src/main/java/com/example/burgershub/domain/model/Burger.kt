91e238d3c4377241ed8b630cc94b16285b56b53a
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-24 20:37:33 -0300

    create domain model Burger types

--- app/src/main/java/com/example/burgershub/domain/model/Burger.kt
+++ app/src/main/java/com/example/burgershub/domain/model/Burger.kt
@@ -1,14 +1,11 @@
 package com.example.burgershub.domain.model
 
-import com.example.burgershub.data.model.ImageResponse
-import com.example.burgershub.data.model.IngredientResponse
-
 data class Burger(
     val id: Int? = null,
     val name: String? = null,
-    val images: List<ImageResponse>? = null,
+    val images: List<Image>? = null,
     val desc: String? = null,
-    val ingredients: List<IngredientResponse>? = null,
+    val ingredients: List<Ingredient>? = null,
     val price: Float? = null,
     val veg: Boolean? = null
 )
