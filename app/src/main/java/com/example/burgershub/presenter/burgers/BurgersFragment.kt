d7772acf68483d2265ea118c6a84b68315f67556
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-25 16:09:04 -0300

    listing burger items on recycler view, by getBurgers and initRecyclerView on BurgersFragment

--- app/src/main/java/com/example/burgershub/presenter/burgers/BurgersFragment.kt
+++ app/src/main/java/com/example/burgershub/presenter/burgers/BurgersFragment.kt
@@ -1,12 +1,15 @@
 package com.example.burgershub.presenter.burgers
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.burgershub.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.burgershub.databinding.FragmentBurgersBinding
+import com.example.burgershub.domain.model.Burger
+import com.example.burgershub.util.StateView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +18,8 @@ class BurgersFragment : Fragment() {
     private var _binding: FragmentBurgersBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: BurgerViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +27,41 @@ class BurgersFragment : Fragment() {
     ): View {
         _binding = FragmentBurgersBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getBurgers()
+    }
+
+    private fun getBurgers() {
+        viewModel.getBurgers().observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
+                is StateView.Loading -> {
+                }
+                is StateView.Success -> {
+                    binding.progressBar.visibility = View.GONE
+
+                    val burgers = stateView.data ?: emptyList()
+                    initRecyclerView(burgers)
+                }
+                is StateView.Error -> {
+                    binding.progressBar.visibility = View.GONE
+
+                    Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun initRecyclerView(burgers: List<Burger>) {
+        with(binding.rvBurgers) {
+            setHasFixedSize(true)
+            adapter = BurgersAdapter(burgers) { burgerId ->
+
+            }
+        }
     }
 
     override fun onDestroyView() {
