de6e92263126518eff8ebf1c16313c9a42d94f5c
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-25 16:44:20 -0300

    implement NetworkModule; adjust ServiceProvider and AndroidManifest.xml to connect with api

--- app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
+++ app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
@@ -1,6 +1,8 @@
 package com.example.burgershub.network
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -13,6 +15,22 @@ class ServiceProvider {
     private val client = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .addInterceptor(Interceptor { chain ->
+            chain.run {
+                proceed(
+                    request()
+                        .newBuilder()
+                        .addHeader(
+                            name = "X-RapidAPI-Key",
+                            value = "mock key"
+                        )
+                        .build()
+                )
+            }
+        })
         .build()
 
     private val retrofit: Retrofit = Retrofit.Builder()
